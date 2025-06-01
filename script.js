4d0702a99645efc2783ccf3c7cbdf1754656c60a
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-06-01 15:01:43 +0200

    reducer and state change

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function orderPizza() {
     shop_name: "Pizza Shop",
   };
 }
+
+//Reducer
+const initialState = {
+  pizzaBase: 100,
+  //   toppings: ["cheese", "capsicum"],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Order_Pizza:
+      return {
+        ...state, //for when an object has a lot of keys we copy the whole object
+        pizzaBase: pizzaBase - 1, //and then only update what we need
+      };
+    default:
+      return state;
+  }
+};
