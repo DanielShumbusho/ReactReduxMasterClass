293c1eab825223f487b360fb5e2c9201f53e50e5
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-06-01 14:51:19 +0200

    action and action creator

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("welcome to redux");
+// console.log("welcome to redux");
+
+const Order_Pizza = "Order_Pizza";
+//Action in Redux
+// const action = {
+//   type: Order_Pizza,
+//   shop_name: "Pizza Shop",
+// };
+
+//Action creator
+function orderPizza() {
+  return {
+    type: Order_Pizza,
+    shop_name: "Pizza Shop",
+  };
+}
