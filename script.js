a8684aa5df5210ac08dfe4499fcae3b1ee7ae26a
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-06-01 15:38:21 +0200

    dispatch and subscription

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const redux = require("redux");
+const createStore = redux.createStore;
 // console.log("welcome to redux");
 
 const Order_Pizza = "Order_Pizza";
@@ -26,9 +28,31 @@ const reducer = (state = initialState, action) => {
     case Order_Pizza:
       return {
         ...state, //for when an object has a lot of keys we copy the whole object
-        pizzaBase: pizzaBase - 1, //and then only update what we need
+        pizzaBase: state.pizzaBase - 1, //and then only update what we need
       };
     default:
       return state;
   }
 };
+
+//Store
+//1- Store needs to hold the application states
+
+const store = createStore(reducer); //because the reducer has the state and action
+
+//2-Exposes the getState method which gives accesss to th ecurrent state in the store
+console.log("Initial State:", store.getState()); //will print the initial state of initialState
+
+//3-Register listeners via subscribe
+const unsubscribe = store.subscribe(() =>
+  console.log("Update State", store.getState())
+);
+
+//4- update state via dispatch (action)
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+store.dispatch(orderPizza());
+unsubscribe();
+store.dispatch(orderPizza());
