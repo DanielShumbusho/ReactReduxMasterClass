884f64dccb5be9849717ba79a4aeef02dd626ed8
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-06-04 18:02:12 +0200

    asynchronous operations

--- ReduxToolkit/index.js
+++ ReduxToolkit/index.js
@@ -1,10 +1,12 @@
 const store = require("./app/store");
+const { fetchProducts } = require("./features/products/productSlice");
 const pizzaActions = require("./features/pizza/pizzaSlice").pizzaActions;
 const burgerAction = require("./features/burger/burgerSlice").burgerAction;
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
   //   console.log("Updated state", store.getState());
 });
-store.dispatch(pizzaActions.pizza_order());
-store.dispatch(burgerAction.burger_order());
+store.dispatch(fetchProducts());
+// store.dispatch(pizzaActions.pizza_order());
+// store.dispatch(burgerAction.burger_order());
 unsubscribe();
