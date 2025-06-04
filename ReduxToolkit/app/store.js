464dae4a424ca318b83b8f12a005d51bbe447b08
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-06-04 17:16:54 +0200

    middleware logger added

--- ReduxToolkit/app/store.js
+++ ReduxToolkit/app/store.js
@@ -1,10 +1,13 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const pizzaReducer = require("../features/pizza/pizzaSlice");
 const burgerReducer = require("../features/burger/burgerSlice");
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 const store = configureStore({
   reducer: {
     pizza: pizzaReducer,
     burger: burgerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 module.exports = store;
--- ReduxToolkit/index.js
+++ ReduxToolkit/index.js
@@ -3,7 +3,7 @@ const pizzaActions = require("./features/pizza/pizzaSlice").pizzaActions;
 const burgerAction = require("./features/burger/burgerSlice").burgerAction;
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
-  console.log("Updated state", store.getState());
+  //   console.log("Updated state", store.getState());
 });
 store.dispatch(burgerAction.burger_order());
 store.dispatch(pizzaActions.pizza_order());
